b1b0b2323bf9244faa73615315ebdfe0b47f9a4d
dimasdananjaya <gededimas@gmail.com>
2020-05-20 22:09:43 +0800

    Added 2 New Section

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -163,6 +163,46 @@
                     ]
                     });
         
-        </script>
+    </script>
+
+    <script>
+    $(".most-popular-carousel").slick({
+                arrows: true,
+                infinite: false,
+                speed: 150,
+                dots: true,
+                autoplay: true,
+                autoplaySpeed: 3000,
+                slidesToShow: 4,
+                slidesToScroll: 3,
+                responsive: [{
+                    breakpoint: 1024,
+                    settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    arrows: true
+                    }
+                }, {
+                    breakpoint: 600,
+                    settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    arrows: true
+                    }
+                }, {
+                    breakpoint: 480,
+                    settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: true
+                    }
+                } // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+                ]
+                });
+    
+    </script>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
       <div class="col-lg-12 col-lg-8 col-xl-6 text-center">
         <img alt="image" width="200" src="images/logo/fat-cat-logo.png">
         <h2>Girls Fashion</h2>
-        <p>#fashion #koreanfashion #fashiondenpasar</p>
+        <p>#fashion #koreanfashion #onlinefashion</p>
       </div><!--end col-->
     </div><!--end row-->
   </div><!--end container-->
@@ -41,66 +41,130 @@
   </div>
 </section>
 
-<section id="product">
+<section id="newly-arrived">
     <div class="container">
         <h3>Newly Arrived</h3>
+        <hr>
         <div class="newly-arrived-carousel">
             <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="..." alt="Card image cap">
+                <img class="card-img-top img-responsive" src="images/product/sample1.jpeg" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text">Price : Rp 10.000</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div><!--end card-->
 
             <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="..." alt="Card image cap">
+              <img class="card-img-top img-responsive" src="images/product/sample2.jpeg" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text">Price : Rp 10.000</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div><!--end card-->
 
             <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="..." alt="Card image cap">
+              <img class="card-img-top img-responsive" src="images/product/sample3.jpeg" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text">Price : Rp 10.000</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div><!--end card-->
 
             <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="..." alt="Card image cap">
+              <img class="card-img-top img-responsive" src="images/product/sample3.jpeg" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text">Price : Rp 10.000</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div><!--end card-->
 
             <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="..." alt="Card image cap">
+              <img class="card-img-top img-responsive" src="images/product/sample3.jpeg" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text">Price : Rp 10.000</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div><!--end card-->
 
             <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="..." alt="Card image cap">
+              <img class="card-img-top img-responsive" src="images/product/sample3.jpeg" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text">Price : Rp 10.000</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div><!--end card-->
         </div><!--end carousel-->
+        <a href="#" class="btn-view-all btn btn-primary">View All</a>
     </div><!--end container-->
 </section>
 
+<section id="most-popular">
+  <div class="container">
+      <h3>Most Popular</h3>
+      <hr>
+      <div class="most-popular-carousel">
+          <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="..." alt="Card image cap">
+              <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Product X</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+          </div><!--end card-->
+
+          <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="..." alt="Card image cap">
+              <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Product X</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+          </div><!--end card-->
+
+          <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="..." alt="Card image cap">
+              <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Product X</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+          </div><!--end card-->
+
+          <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="..." alt="Card image cap">
+              <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Product X</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+          </div><!--end card-->
+
+          <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="..." alt="Card image cap">
+              <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Product X</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+          </div><!--end card-->
+
+          <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="..." alt="Card image cap">
+              <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Product X</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+          </div><!--end card-->
+      </div><!--end carousel-->
+      <a href="#" class="btn-view-all btn btn-primary">View All</a>
+  </div><!--end container-->
+</section>
 
 @endsection
